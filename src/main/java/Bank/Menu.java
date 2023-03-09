e23fa9d7e2ed17dd9587a8ac47d1f248a5875ce9
Rasa Arnaityte <r.arnaityte@gmail.com>
2023-03-10 00:00:31 +0200

    small changes in the main

--- src/main/java/Bank/Menu.java
+++ src/main/java/Bank/Menu.java
@@ -1,7 +1,87 @@
 package Bank;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Menu {
-    protected static void bankEmployeeMenu() {
+
+    protected static void MenuCall (){
+        UserRegAndLogin userRegAndLogin = new UserRegAndLogin();
+
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Welcome to the CatBank! Please press 1 if you are employee and 2 if you are customer");
+
+
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+
+        while (true){
+            try {
+                choice = scanner.nextInt();
+
+                if (choice == 1) {
+                    Menu.bankEmployeeMenu();
+
+
+                    int employeeChoice = 0;
+                    do { //try catch block and do while loop to validate user input and not stop program with invalid input
+                        try {
+                            employeeChoice= scanner.nextInt();
+                            if (employeeChoice < 1 || employeeChoice > 4) {
+                                throw new InputMismatchException();
+                            }
+                            switch (employeeChoice) {
+                                case 1 -> Bank.updatePersonalInfo();
+                                case 2 -> Bank.deleteUser();
+                                case 3 -> Bank.findUser();
+                                case 4 -> Bank.changeUserBalance();
+                                default -> System.out.println("Invalid choice. Please try again.");
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println("Invalid choice entered. Please enter a number between 1 and 4.");
+                        }
+                    }while (employeeChoice < 1 || employeeChoice > 4);
+                    scanner.nextLine();
+
+
+                } else if (choice == 2) {
+                    Menu.customerMenu();
+
+
+                    int customerChoice=0; // try catch block and do while loop to validate user input and not stop program with invalid input
+                    do { //try catch block and do while loop to validate user input and not stop program with invalid input
+                        try {
+                            customerChoice= scanner.nextInt();
+                            if (customerChoice != 1 && customerChoice != 2 && customerChoice != 3 ) {
+                                throw new InputMismatchException();
+                            }
+                            switch (customerChoice) {
+                                case 1 -> userRegAndLogin.loginUser();
+                                case 2 -> UserRegAndLogin.registerUser();
+                                case 3 -> UserRegAndLogin.changeBalance();
+
+                                default -> System.out.println("Invalid choice. Please reconnect and try again.");
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println("Invalid input. Try again");
+                        }
+                    }while (customerChoice != 1 && customerChoice != 2 && customerChoice != 3);
+                    scanner.nextLine();
+
+
+                } else {
+                    throw new InputMismatchException();
+                } break; //stop while loop if input is valid
+
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter 1 or 2.");
+                scanner.nextLine();
+            }
+        }
+
+    }
+    private static void bankEmployeeMenu() {
         System.out.println("Please choose action:");
         System.out.println("1- update user's personal information");
         System.out.println("2- remove user");
@@ -9,7 +89,7 @@ public class Menu {
         System.out.println("4- change balance for user");
 
     }
-    protected static void customerMenu(){
+    private static void customerMenu(){
         System.out.println("Please choose action:");
         System.out.println("1 - log in and check balance");
         System.out.println("2 - register");
